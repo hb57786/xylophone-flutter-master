9b6404d89ac6bec6e35cde95960f256d55570177
hb57786 <tadeufcad@gmail.com>
2021-05-30 08:10:04 -0300

    add playSoung function

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int playSound) {
+    final player = AudioCache();
+    player.play('note$playSound.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: [
+              // ignore: deprecated_member_use
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  playSound(1);
+                },
+              ),
+              // ignore: deprecated_member_use
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  playSound(2);
+                },
+              ),
+              // ignore: deprecated_member_use
+              FlatButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  playSound(3);
+                },
+              ),
+              // ignore: deprecated_member_use
+              FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  playSound(4);
+                },
+              ),
+              // ignore: deprecated_member_use
+              FlatButton(
+                color: Colors.teal,
+                onPressed: () {
+                  playSound(5);
+                },
+              ),
+              // ignore: deprecated_member_use
+              FlatButton(
+                color: Colors.blue,
+                onPressed: () {
+                  playSound(6);
+                },
+              ),
+              // ignore: deprecated_member_use
+              FlatButton(
+                color: Colors.purple,
+                onPressed: () {
+                  playSound(7);
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
